f563e6d7256dfa264f9bd0b1c025b06571950d5c
johanlia000 <lxjohansen@gmail.com>
2017-05-22 09:38:38 -0700

    changes

--- src/Game.java
+++ src/Game.java
@@ -1,6 +1,7 @@
 import acm.graphics.*;     // GOval, GRect, etc.
 import acm.program.*;      // GraphicsProgram
 import acm.util.*;         // RandomGenerator
+
 import java.applet.*;      // AudioClip
 import java.awt.*;         // Color
 import java.awt.event.*;   // MouseEvent
@@ -11,12 +12,20 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 
-public class Game
+import com.sun.prism.paint.Color;
+
+public class Game extends acm.program.GraphicsProgram
 {
 	public static void main(String[] args) 
 	{
+		int width = 1280;
+		int height = 750;
+		int count = 0;
+		
 	    GCanvas gc = new GCanvas();
+	    gc.setBackground(java.awt.Color.BLACK);
         JFrame frame = new JFrame();
+        frame.setSize(width, height);
         
         int height = 720;
         int width = 1280;
@@ -25,8 +34,75 @@ public class Game
         
         frame.getContentPane().add(BorderLayout.CENTER, gc);
         GImage startScreen = new GImage("start.png");
-        gc.add(startScreen);
-        frame.show();
         
+        frame.show();
+        frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
+        
+        while (count < 51) {
+        	setOpacity(startScreen, count*5);
+        	gc.add(startScreen);
+        	startScreen.pause(50);
+        	count++;
+        }
+        
+        //spacebar start code
+        startScreen.pause(2000);
+        
+        while (count > 0) {
+        	setOpacity(startScreen, count*5);
+        	gc.add(startScreen);
+        	startScreen.pause(50);
+        	count--;
+        }
+        
+        gc.remove(startScreen);
+        
+	}
+	
+	public static void setOpacity(GImage source, int alpha) {
+		int[][] pixels = source.getPixelArray();
+		
+		int[][] newpixels = new int[pixels.length][pixels[0].length];
+		
+		for (int r = 0; r < pixels.length; r++) {
+			for (int c = 0; c < pixels[r].length; c++) {
+				try {
+					int red = GImage.getRed(pixels[r][c]);
+					int blue = GImage.getBlue(pixels[r][c]);
+					int green = GImage.getGreen(pixels[r][c]);
+					
+					newpixels[r][c] = GImage.createRGBPixel(red, green, blue, alpha);
+				}
+				catch (ArrayIndexOutOfBoundsException e) {
+				}
+			}
+		}
+		source.setPixelArray(newpixels);
+	}
+	
+	private class player extends GImage {
+
+		public player(Image image, double x, double y) {
+			super(image, x, y);
+			// TODO Auto-generated constructor stub
+		}
+		
+	}
+	
+	private class healthbar extends GObject
+	{
+
+		@Override
+		public GRectangle getBounds() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		protected void paint2d(Graphics2D g) {
+			// TODO Auto-generated method stub
+			
+		}
+		//Contains the 
 	}
 }
