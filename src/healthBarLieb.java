4ba6fc8bb85c3bbb43d7572ec44dc3efbfaf9fb1
johanlia000 <lxjohansen@gmail.com>
2017-05-26 08:38:52 -0700

    changes

---
+++ src/healthBarLieb.java
@@ -0,0 +1,24 @@
+import acm.graphics.GRect;
+import acm.graphics.GRectangle;
+import java.awt.Graphics2D;
+
+public class healthBarLieb extends GRect{
+    
+    
+    public healthBarLieb(double arg0, double arg1, double arg2, double arg3) {
+        super(arg0, arg1, arg2, arg3);
+        // TODO Auto-generated constructor stub
+    }
+
+
+    public GRectangle getBounds() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    
+    protected void paint2d(Graphics2D g) {
+        // TODO Auto-generated method stub
+        
+    }
+}
